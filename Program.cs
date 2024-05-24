95e88155f124fde82b3cf12a785adfbf8a5d2367
GrinchenkoAnna <grinchenkoanna@inbox.ru>
2024-05-25 01:46:30 +0700

    Refactored the choosing part and started solving the backpack problem

--- Program.cs
+++ Program.cs
@@ -1,46 +1,52 @@
 ﻿using System.IO;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
 int N = 0, M = 1;
 const int output_width = 5;
+int[,] array_2d = new int[0, 0];
+int[] array_1d = [];
+bool positive = false;
+int range = 20;
+int weight = 0;
 
 #region Select
-void SelectSort(int[] array)
+void SelectSort()
 {
     int comparision = 0, permutation = 0;
 
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array_1d.Length; i++)
     {
         int min = i;
-        for (int j = i + 1; j < array.Length; j++)
+        for (int j = i + 1; j < array_1d.Length; j++)
         {
-            if (array[j] < array[min]) min = j;
+            if (array_1d[j] < array_1d[min]) min = j;
             comparision++;
         }
-        (array[i], array[min]) = (array[min], array[i]);
+        (array_1d[i], array_1d[min]) = (array_1d[min], array_1d[i]);
         permutation++;
     }
 
     Console.WriteLine("\u2193");
-    Print1DArray(array);
+    Print1DArray();
 
-    Console.WriteLine($"Теоретическая трудоемкость алгоритма: 1/2*N^2 = {Math.Ceiling((double)(Math.Pow(array.Length, 2)) / 2)}");
+    Console.WriteLine($"Теоретическая трудоемкость алгоритма: 1/2*N^2 = {Math.Ceiling((double)(Math.Pow(array_1d.Length, 2)) / 2)}");
     Console.WriteLine($"Реальная трудоемкость алгоритма: {comparision + permutation}");
 }
 #endregion
 
 #region Bubble
-void BubbleSort(int[] array)
+void BubbleSort()
 {
     int comparision = 0, permutation = 0;
 
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 0; i < array_1d.Length; i++)
     {
-        for (int j = 0; j < array.Length - 1 - i; j++)
+        for (int j = 0; j < array_1d.Length - 1 - i; j++)
         {
-            if (array[j] > array[j + 1])
+            if (array_1d[j] > array_1d[j + 1])
             {
-                (array[j], array[j + 1]) = (array[j + 1], array[j]);
+                (array_1d[j], array_1d[j + 1]) = (array_1d[j + 1], array_1d[j]);
                 permutation++;
             }
             comparision++;
@@ -48,34 +54,34 @@ void BubbleSort(int[] array)
     }
 
     Console.WriteLine("\u2193");
-    Print1DArray(array);
+    Print1DArray();
 
-    Console.WriteLine($"Теоретическая трудоемкость алгоритма: N^2 = {Math.Pow(array.Length, 2)}");
+    Console.WriteLine($"Теоретическая трудоемкость алгоритма: N^2 = {Math.Pow(array_1d.Length, 2)}");
     Console.WriteLine($"Реальная трудоемкость алгоритма: {comparision + permutation}");
 }
 #endregion
 
 #region Merge
-void MergeSort(int[] array)
+void MergeSort()
 {
     int comparision = 0, permutation = 0;
-    int delete = CheckN(ref array);
+    int delete = CheckN();
     Console.WriteLine($"delete = {delete}");
-    Sort(array, 0, array.Length - 1, ref comparision, ref permutation);
+    Sort(0, array_1d.Length - 1, ref comparision, ref permutation);
 
     Console.WriteLine("\u2193");
-    Print1DArray(array);
+    Print1DArray();
     if (delete != 0)
     {
         Console.WriteLine("Удаление лишних элементов:");
-        Array.Resize<int>(ref array, array.Length - delete);
-        Print1DArray(array);
+        Array.Resize(ref array_1d, array_1d.Length - delete);
+        Print1DArray();
     }
 
-    Console.WriteLine($"Теоретическая трудоемкость алгоритма: N*logN = {array.Length * Math.Ceiling(Math.Log2(array.Length))}");
+    Console.WriteLine($"Теоретическая трудоемкость алгоритма: N*logN = {array_1d.Length * Math.Ceiling(Math.Log2(array_1d.Length))}");
     Console.WriteLine($"Реальная трудоемкость алгоритма: {comparision + permutation}");
 }
-void Sort(int[] array, int left, int right, ref int comparision, ref int permutation)
+void Sort(int left, int right, ref int comparision, ref int permutation)
 {
 
     if (left < right)
@@ -83,14 +89,14 @@ void Sort(int[] array, int left, int right, ref int comparision, ref int permuta
         comparision++;
 
         int middle = left + (right - left) / 2;
-        Sort(array, left, middle, ref comparision, ref permutation);
-        Sort(array, middle + 1, right, ref comparision, ref permutation);
-        Merge(array, left, middle, right, comparision, permutation);
+        Sort(left, middle, ref comparision, ref permutation);
+        Sort(middle + 1, right, ref comparision, ref permutation);
+        Merge(left, middle, right, comparision, permutation);
 
         permutation++;
     }
 }
-void Merge(int[] array, int left, int middle, int right, int comparision, int permutation)
+void Merge(int left, int middle, int right, int comparision, int permutation)
 {
     int size1 = middle - left + 1;
     int size2 = right - middle;
@@ -99,65 +105,65 @@ void Merge(int[] array, int left, int middle, int right, int comparision, int pe
     List<int> tempLeft = [];
     List<int> tempRight = [];
 
-    for (i = 0; i < size1; i++) tempLeft.Add(array[left + i]);
-    for (j = 0; j < size2; j++) tempRight.Add(array[middle + 1 + j]);
+    for (i = 0; i < size1; i++) tempLeft.Add(array_1d[left + i]);
+    for (j = 0; j < size2; j++) tempRight.Add(array_1d[middle + 1 + j]);
 
     i = 0; j = 0;
     int k = left;
 
     while (i < size1 && j < size2)
     {
-        if (tempLeft[i] <= tempRight[j]) { array[k] = tempLeft[i]; i++; permutation++; }
-        else { array[k] = tempRight[j]; j++; permutation++; }
+        if (tempLeft[i] <= tempRight[j]) { array_1d[k] = tempLeft[i]; i++; permutation++; }
+        else { array_1d[k] = tempRight[j]; j++; permutation++; }
         k++; comparision++;
     }
-    while (i < size1) { array[k] = tempLeft[i]; i++; k++; permutation++; }
-    while (j < size2) { array[k] = tempRight[j]; j++; k++; permutation++; }
+    while (i < size1) { array_1d[k] = tempLeft[i]; i++; k++; permutation++; }
+    while (j < size2) { array_1d[k] = tempRight[j]; j++; k++; permutation++; }
 }
-int CheckN(ref int[] array)
+int CheckN()
 {
     for (int i = 1; ; i++)
     {
-        if (array.Length == Math.Pow(2, i))
+        if (array_1d.Length == Math.Pow(2, i))
         {
-            Console.WriteLine($"Число элементов в массиве = {array.Length} = 2^k, добавлять элементы не требуется");
+            Console.WriteLine($"Число элементов в массиве = {array_1d.Length} = 2^k, добавлять элементы не требуется");
             return 0;
         }
-        else if (array.Length < Math.Pow(2, i))
+        else if (array_1d.Length < Math.Pow(2, i))
         {
-            int to_add = (int)Math.Pow(2, i) - array.Length;
-            int prev_length = array.Length;
-            Console.WriteLine($"Число элементов в массиве = {array.Length} != 2^k, необходимо добавить {to_add}");
-            int M = FindMax(array);
-            Array.Resize<int>(ref array, array.Length + to_add);
-            for (int j = 1; j <= to_add; j++) array[prev_length - 1 + j] = M + j; 
+            int to_add = (int)Math.Pow(2, i) - array_1d.Length;
+            int prev_length = array_1d.Length;
+            Console.WriteLine($"Число элементов в массиве = {array_1d.Length} != 2^k, необходимо добавить {to_add}");
+            int M = FindMax();
+            Array.Resize(ref array_1d, array_1d.Length + to_add);
+            for (int j = 1; j <= to_add; j++) array_1d[prev_length - 1 + j] = M + j; 
             Console.WriteLine("Результат:");
-            Print1DArray(array);
+            Print1DArray();
 
             return to_add;
         }
     }
 }
-int FindMax(int[] array)
+int FindMax()
 {
-    int max = array[0];
-    for (int i = 0; i < array.Length; i++)
+    int max = array_1d[0];
+    for (int i = 0; i < array_1d.Length; i++)
     {
-        if (array[i] > max) max = array[i];
+        if (array_1d[i] > max) max = array_1d[i];
     }
     return max;
 }
 #endregion
 
 #region Stairs
-void Stairs(int[] array)
+void Stairs()
 {
     int[] summa = new int[N];
     int[] path = new int[N];
 
-    summa[0] = array[0];
-    summa[1] = Math.Max(array[1] + summa[0], array[1]);
-    for (int i = 2; i < N; i++) summa[i] = array[i] + Math.Max(summa[i - 1], summa[i - 2]);
+    summa[0] = array_1d[0];
+    summa[1] = Math.Max(array_1d[1] + summa[0], array_1d[1]);
+    for (int i = 2; i < N; i++) summa[i] = array_1d[i] + Math.Max(summa[i - 1], summa[i - 2]);
 
     path[0] = N - 1;
     int path_index = 1;
@@ -173,80 +179,132 @@ void Stairs(int[] array)
     Array.Reverse(path);
 
     Console.WriteLine("Путь: ");
-    PrintPath_1DArray(array, path);
+    PrintPath_1DArray(path);
     Console.WriteLine($"Сумма: {summa[N - 1]}");
 }
 #endregion
 
 #region ChessBoard
-void ChessBoard(int[,] array)
+void ChessBoard()
 {
     int[,] summa = new int[M, N]; 
-    Path[] path = new Path[M + N - 1];
+    Union[] path = new Union[M + N - 1];
     
     for (int i = 0; i < M; i++)
     {
         for (int j = 0; j < N; j++)
         {
-            if (i > 0 && j > 0) summa[i, j] = array[i, j] + Math.Max(summa[i, j - 1], summa[i - 1, j]);
-            else if (i > 0 && j == 0) summa[i, j] = array[i, j] + summa[i - 1, j];
-            else if (i == 0 && j > 0) summa[i, j] = array[i, j] + summa[i, j - 1];
-            else summa[0, 0] = array[0, 0];
+            if (i > 0 && j > 0) summa[i, j] = array_2d[i, j] + Math.Max(summa[i, j - 1], summa[i - 1, j]);
+            else if (i > 0 && j == 0) summa[i, j] = array_2d[i, j] + summa[i - 1, j];
+            else if (i == 0 && j > 0) summa[i, j] = array_2d[i, j] + summa[i, j - 1];
+            else summa[0, 0] = array_2d[0, 0];
         }
     }
 
-    path[0].path_x = M - 1;
-    path[0].path_y = N - 1;
+    path[0].first_member = M - 1;
+    path[0].second_member = N - 1;
     int path_index = 1, x = M - 1, y = N - 1;
     while ((x != 0 || y != 0) && path_index < M + N - 2)
     {
-        path[path_index].path_x = x;
-        path[path_index].path_y = y;
+        path[path_index].first_member = x;
+        path[path_index].second_member = y;
 
         if (x > 0 && y > 0)
         {
-            if (summa[x, y - 1] >= summa[x - 1, y]) { path[path_index].path_y -= 1; y -= 1; }
-            else { path[path_index].path_x -= 1; x -= 1; }
+            if (summa[x, y - 1] >= summa[x - 1, y]) { path[path_index].second_member -= 1; y -= 1; }
+            else { path[path_index].first_member -= 1; x -= 1; }
         }
-        else if (x > 0 && y == 0) { path[path_index].path_x -= 1; x -= 1; }
-        else if (x == 0 && y > 0) { path[path_index].path_y -= 1; y -= 1; }
+        else if (x > 0 && y == 0) { path[path_index].first_member -= 1; x -= 1; }
+        else if (x == 0 && y > 0) { path[path_index].second_member -= 1; y -= 1; }
 
         path_index++;
     }
-    path[path_index].path_x = 0;
-    path[path_index].path_y = 0;
+    path[path_index].first_member = 0;
+    path[path_index].second_member = 0;
 
     Array.Reverse(path);
 
     Console.WriteLine("Путь: ");
-    PrintPath_2DArray(array, path);
+    PrintPath_2DArray(path);
     Console.WriteLine($"Сумма: {summa[M - 1, N - 1]}");
 
 }
 #endregion
-void Print1DArray(int[] array)
+
+#region Basackpack
+void Backpack(int max_weight)
+{    
+    //Union = weight, price
+    Union[] items = new Union[max_weight];
+    List<int> candidats = new List<int>();
+    int unit = 0;
+    int filled = 0;
+
+    for (int j = 0; j < N; j++)
+    {
+        items[j].first_member = array_2d[0, j];
+        items[j].second_member = array_2d[1, j];
+    }
+    Print2DArray();
+
+    filled = N;
+    //ничего не добавляется...
+    for (int k = array_2d[0, N - 1] + 1; k <= max_weight; k++)
+    {
+        for (int j = 0; j < filled; j++)
+        {
+            unit = items[j].first_member;
+            Console.WriteLine($"unit = {unit}, k - unit = {k - unit}");
+            if (items.Any(x => x.first_member == k - unit))
+            {
+                candidats.Add(items[k - unit].second_member + items[unit].second_member);
+            }
+        }
+
+        foreach (Union item in items)
+        {
+            Console.Write($"weight - {item.first_member}, price - {item.second_member}");
+            Console.WriteLine();
+        }
+
+        if (candidats.Count != 0)
+        {
+            items[k].second_member = candidats.Max();
+            filled++;
+            candidats.Clear();
+        }            
+    }
+
+    foreach (Union item in items) 
+    { 
+        Console.Write($"weight - {item.first_member}, price - {item.second_member}");
+        Console.WriteLine();
+    }
+}
+#endregion
+void Print1DArray()
 {
     Console.ForegroundColor = ConsoleColor.Red;
-    for (int i = 0; i < array.Length; i++) Console.Write($"{array[i]}".PadRight(output_width));
+    for (int i = 0; i < array_1d.Length; i++) Console.Write($"{array_1d[i]}".PadRight(output_width));
     Console.ResetColor();
     Console.WriteLine();
 }
 
-void Print2DArray(int[,] array)
+void Print2DArray()
 {
     Console.ForegroundColor = ConsoleColor.Red;
     for (int i = 0; i < M; i++)
     {
-        for (int j = 0; j < N; j++) Console.Write($"{array[i, j]}".PadRight(output_width));
+        for (int j = 0; j < N; j++) Console.Write($"{array_2d[i, j]}".PadRight(output_width));
         Console.WriteLine();
     }
     Console.ResetColor();
     Console.WriteLine();
 }
 
-void PrintPath_1DArray(int[] array, int[] path)
+void PrintPath_1DArray(int[] path)
 {
-    for (int i = 0, k = 0; i < array.Length; i++)
+    for (int i = 0, k = 0; i < array_1d.Length; i++)
     {
         if (i == path[k])
         {
@@ -254,36 +312,33 @@ void PrintPath_1DArray(int[] array, int[] path)
             k++;
         }
         else Console.ForegroundColor = ConsoleColor.Red;
-        Console.Write($"{array[i]}".PadRight(5));
+        Console.Write($"{array_1d[i]}".PadRight(5));
     }
     Console.ResetColor();
     Console.WriteLine();
 }
 
-void PrintPath_2DArray(int[,] array, Path[] path)
+void PrintPath_2DArray(Union[] path)
 {
+    //Union = x, y
     int k = 0;
     for (int i = 0; i < M && k < M + N - 1; i++)
     {
         for (int j = 0; j < N && k < M + N - 1; j++)
         {
-            if (i == path[k].path_x && j == path[k].path_y)
+            if (i == path[k].first_member && j == path[k].second_member)
             {
                 Console.ForegroundColor = ConsoleColor.Green;
                 k++;
             }
             else Console.ForegroundColor = ConsoleColor.Red;
-            Console.Write($"{array[i, j]}".PadRight(output_width));
+            Console.Write($"{array_2d[i, j]}".PadRight(output_width));
         }
         Console.WriteLine();
     }    
     Console.ResetColor();
     Console.WriteLine();
 }
-
-
-int[,] array_2d = new int[0, 0];
-int[] array_1d = [];
 
 Console.OutputEncoding = System.Text.Encoding.UTF8;
 
@@ -294,76 +349,120 @@ while (true)
     string choice = Console.ReadLine();
     Console.WriteLine();
 
+    if (positive == true) positive = false;
+
     switch (choice)
     {
         case "1":
             Console.WriteLine("SelectSort");
-            PrepareMethod(1, () => SelectSort(array_1d));
+            PrepareData(1);
+            SelectSort();
             break;
 
         case "2":
             Console.WriteLine("BubbleSort");
-            PrepareMethod(1, () => BubbleSort(array_1d));
+            PrepareData(2);
+            BubbleSort();
             break;
 
         case "3":
             Console.WriteLine("MergeSort");
-            PrepareMethod(1, () => MergeSort(array_1d));
+            PrepareData(3);
+            MergeSort();
             break;
 
         case "4":
             Console.WriteLine("Лестница");
-            PrepareMethod(1, () => Stairs(array_1d));
+            PrepareData(4);
+            Stairs();
             break;
 
         case "5":
             Console.WriteLine("Шахматная доска");
-            PrepareMethod(2, () => ChessBoard(array_2d));
+            PrepareData(5);
+            ChessBoard();
+            break;
+
+        case "6":
+            Console.WriteLine("Рюкзак");
+            positive = true;
+            Backpack(PrepareData(6));
             break;
 
         case "0": return;
 
         default: break;
     }
-}
-
-void PrepareMethod(int dimension, Action method)
-{
-    N = 0; M = 1;
-    Random random = new();
-    if (dimension > 1)
-    {
-        Console.Write("Введите M: ");
-        M = Convert.ToInt16(Console.ReadLine());
-    }
-    Console.Write("Введите N: ");
-    N = Convert.ToInt16(Console.ReadLine());
-
-    if (dimension > 1)
-    {
-        Array.Clear(array_2d);
-        array_2d = new int[M, N];
-        for (int i = 0; i < M; i++)
-        {
-            for (int j = 0; j < N; j++) array_2d[i, j] = random.Next(-10, 10);
-        }
-        Print2DArray(array_2d);
-        if (array_2d.Length >= 2) method();
-    }
-    else
-    {
-        Array.Clear(array_1d);
-        array_1d = new int[N];
-        for (int i = 0; i < N; i++) array_1d[i] = random.Next(-10, 10);
-        Print1DArray(array_1d);
-        if (array_1d.Length >= 2) method();
-    }
-
     Console.ReadKey();
 }
 
-struct Path
+int PrepareData(int choice)
 {
-    public int path_x;
-    public int path_y;
+    N = 0; M = 1; weight = 0;
+    //Console.Write("Введите N (количество столбцов): ");
+    //N = Convert.ToInt16(Console.ReadLine());
+
+    Random random = new();
+    int right_border, left_border;
+    switch (positive)
+    {
+        case false:
+            right_border = -10; break;
+
+        case true:
+            right_border = 1; break;
+    }
+    left_border = right_border + range;
+
+    switch (choice)
+    {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+            Array.Clear(array_1d);
+            array_1d = new int[N];
+            for (int i = 0; i < N; i++) array_1d[i] = random.Next(right_border, left_border);
+            Print1DArray();
+            break;
+            
+        case 5:
+        case 6:
+            if (choice == 6)
+            {
+                M = 2; N = 3;
+                //Console.Write("Максимальный вес: ");
+                //weight = Convert.ToInt16(Console.ReadLine());
+                weight = 20;
+                array_2d = new int[2, 3];
+                array_2d[0, 0] = 3;
+                array_2d[0, 1] = 5;
+                array_2d[0, 2] = 7;
+                array_2d[1, 0] = 8;
+                array_2d[1, 1] = 14;
+                array_2d[1, 2] = 20;
+                break;
+            }
+            else
+            {
+                Console.Write("Введите M (количество строк): ");
+                M = Convert.ToInt16(Console.ReadLine());
+            } 
+            Array.Clear(array_2d);
+            array_2d = new int[M, N];
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++) array_2d[i, j] = random.Next(right_border, left_border);
+            }
+            Print2DArray();
+            break;
+    }
+
+    return weight;
+}
+
+struct Union
+{
+    public int first_member;
+    public int second_member;
 }
